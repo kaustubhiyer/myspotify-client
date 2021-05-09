795c136f19e93615ca5f73c701dcd0303cd1b06c
Kaustubh Iyer <kaustubhiyer1998@gmail.com>
2021-05-09 22:15:41 +0530

    complete

---
+++ src/Dashboard.js
@@ -0,0 +1,99 @@
+import React from "react";
+import useAuth from "./useAuth";
+import { Container, Form } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-node";
+import TrackSearchResult from "./TrackSearchResult";
+import Player from "./Player";
+import axios from "axios";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: "ea039643673c45e3961acfc5442caea1",
+});
+
+export default function Dashboard({ code }) {
+  const accessToken = useAuth(code);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [playingTrack, setPlayingTrack] = useState();
+  const [lyrics, setLyrics] = useState("");
+
+  function chooseTrack(track) {
+    setPlayingTrack(track);
+    setSearch("");
+    setLyrics("");
+  }
+
+  useEffect(() => {
+    if (!playingTrack) return;
+    axios
+      .get("http://localhost:3001/lyrics", {
+        params: {
+          track: playingTrack.title,
+          artist: playingTrack.artist,
+        },
+      })
+      .then((response) => setLyrics(response.data.lyrics));
+  }, [playingTrack]);
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+    let cancel = false;
+    spotifyApi.searchTracks(search).then((data) => {
+      if (cancel) return;
+      setSearchResults(
+        data.body.tracks.items.map((track) => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, img) => {
+              if (img.height < smallest.height) return img;
+              return smallest;
+            },
+            track.album.images[0]
+          );
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImage.url,
+          };
+        })
+      );
+    });
+
+    return () => (cancel = true);
+  }, [search, accessToken]);
+
+  return (
+    <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
+      <Form.Control
+        type="search"
+        placeholder="Search Songs/Artists"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+        {searchResults.map((track) => (
+          <TrackSearchResult
+            track={track}
+            chooseTrack={chooseTrack}
+            key={track.uri}
+          />
+        ))}
+        {searchResults.length === 0 && (
+          <div className="text-center" style={{ whiteSpace: "pre" }}>
+            {lyrics}
+          </div>
+        )}
+      </div>
+      <div>
+        <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+      </div>
+    </Container>
+  );
+}
--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,19 @@
 import React from "react";
 
-import Container from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=ea039643673c45e3961acfc5442caea1&response_type=code%redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+  "https://accounts.spotify.com/authorize?client_id=ea039643673c45e3961acfc5442caea1&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
-  return <div></div>;
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <a className="btn btn-success btn-lg" href={AUTH_URL}>
+        Login with Spotify
+      </a>
+    </Container>
+  );
 }
